c1c811ea231c56fdca6eecabd30dab1a549e848b
nantoSur <Surnanto.blogs@gmai.com>
2025-07-29 12:06:39 +0700

    Update invoices page

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -3,22 +3,20 @@ import Search from "@/app/ui/search";
 import Table from "@/app/ui/invoices/table";
 import { CreateInvoice } from "@/app/ui/invoices/buttons";
 import { lusitana } from "@/app/ui/fonts";
-import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
-import { fetchInvoicesPages } from "@/app/lib/data";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 
-type SearchParams = {
-  searchParams?: {
+type PageProps = {
+  searchParams?: Promise<{
     query?: string;
     page?: string;
-  };
+  }>;
 };
 
-export default async function Page({ searchParams }: SearchParams) {
-  const query = searchParams?.query ?? "";
-  const currentPage = Number(searchParams?.page ?? 1);
-
-  const totalPages = await fetchInvoicesPages(query);
+export default async function Page({ searchParams }: PageProps) {
+  const resolvedParams = await searchParams;
+  const query = resolvedParams?.query ?? "";
+  const currentPage = Number(resolvedParams?.page) || 1;
 
   return (
     <div className="w-full">
@@ -36,7 +34,7 @@ export default async function Page({ searchParams }: SearchParams) {
       </Suspense>
 
       <div className="mt-5 flex w-full justify-center">
-        <Pagination totalPages={totalPages} />
+        {/* <Pagination query={query} currentPage={currentPage} /> */}
       </div>
     </div>
   );
