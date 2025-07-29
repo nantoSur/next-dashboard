14a32335cad6c9980401cb49c53cb8ba6bb36755
nantoSur <Surnanto.blogs@gmai.com>
2025-07-29 10:43:19 +0700

    Update invoices page

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,11 +7,7 @@ import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
 import { fetchInvoicesPages } from "@/app/lib/data";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams?: { [key: string]: unknown };
-}) {
+export default async function Page({ searchParams }: any) {
   const query =
     typeof searchParams?.query === "string" ? searchParams.query : "";
   const currentPage =
